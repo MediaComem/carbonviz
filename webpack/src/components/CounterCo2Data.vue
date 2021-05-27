7b50783e84e9f5b43ceb6ce4fbc026f38b5f8daf
chabloz <synn.void@gmail.com>
2021-05-27 17:06:23 +0200

    Add color to live consumption counter

--- webpack/src/components/CounterCo2Data.vue
+++ webpack/src/components/CounterCo2Data.vue
@@ -18,7 +18,7 @@ export default {
     <div class="co2">
       <svg width="14" height="14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2.467 7a4.533 4.533 0 109.066 0 4.533 4.533 0 00-9.066 0z" stroke-width="3.5"></path></svg>
     </div>
-    <span>Co2 - <span class="val">{{ formatCo2(co2) }}</span></span>
+    <span>CO<sub>2</sub> - <span class="val">{{ formatCo2(co2) }}</span></span>
     <div class="data">
       <svg width="16" height="16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8 13.212L13.212 8 8 2.788 2.788 8 8 13.212z" stroke-width="3"></path></svg>
     </div>
@@ -47,15 +47,18 @@ export default {
     color: grey;
     font-weight: 400;
   }
-
-
-
-  .co2, .data{
-    stroke: #000;
+  .co2 {
+    stroke: #906C0D;
+  }
+  .data{
+    stroke: #3D4D50;
   }
   @media (prefers-color-scheme: dark) {
-    .co2 svg path, .data svg path{
-      stroke: #bfbfbf;
+    .co2 {
+      stroke: #BBAA70;
+    }
+    .data{
+      stroke: #A3AFB1;
     }
     section {
       color: white;
