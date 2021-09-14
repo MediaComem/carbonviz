16c54002eeb638a015f59d778ff733aea53ba860
Stéphane Lecorney <stephane.lecorney@heig-vd.ch>
2021-09-14 15:14:02 +0200

    Add data analogies

--- webpack/src/components/Analogy.vue
+++ webpack/src/components/Analogy.vue
@@ -5,10 +5,10 @@ import { roundToPrecision } from '../utils/format.js'
 const analogiesCo2 = [
     {
         text: (value) => {
-            const amountPerKwh = 2.790697674;
-            let number = Math.floor(value / amountPerKwh);
+            const kwPerUnit = 2.790697674;
+            let number = Math.floor(value / kwPerUnit);
             if (number < 1) {
-                const percent = Math.ceil(100* value / amountPerKwh);
+                const percent = Math.ceil(100* value / kwPerUnit);
                 return `Running ${percent}% of a marathon`;
             }
             return `Running ${number} marathons`;
@@ -17,8 +17,8 @@ const analogiesCo2 = [
     },
     {
         text: (value) => {
-            const amountPerKwh = 0.0001813953488;
-            let number = Math.floor(value / amountPerKwh);
+            const kwPerUnit = 0.0001813953488;
+            let number = Math.floor(value / kwPerUnit);
             if (number < 1000) {
                 return `Swimming ${number}m in the sea`;
             }
@@ -28,30 +28,30 @@ const analogiesCo2 = [
     },
     {
         text: (value) => {
-            const amountPerKwh = 0.02543604651;
-            let number = Math.floor(value / amountPerKwh);
+            const kwPerUnit = 0.02543604651;
+            let number = Math.floor(value / kwPerUnit);
             return `Biking ${number}km`;
         },
         asset: 'analogy_bicycle.png'
     },
     {
         text: (value) => {
-            const amountPerKwh = 0.25;
-            return `Cooking ${roundToPrecision(value/amountPerKwh, 1)} frozen pizzas`;
+            const kwPerUnit = 0.25;
+            return `Cooking ${roundToPrecision(value/kwPerUnit, 1)} frozen pizzas`;
         },
         asset: 'analogy_frozenpizza.png'
     },
     {
         text: (value) => {
-            const amountPerKwh = 0.116;
-            return `Boiling ${roundToPrecision(value/amountPerKwh, 1)}L water`;
+            const kwPerUnit = 0.116;
+            return `Boiling ${roundToPrecision(value/kwPerUnit, 1)}L water`;
         },
         asset: 'analogy_boilingwater.png'
     },
     {
         text: (value) => {
-            const amountPerKwh = 0.0203488372093023;
-            let number = Math.floor(value / amountPerKwh);
+            const kwPerUnit = 0.0203488372093023;
+            let number = Math.floor(value / kwPerUnit);
             return `Sawing ${number} wood boards`;
         },
         asset: 'analogy_sawing.png'
@@ -60,34 +60,55 @@ const analogiesCo2 = [
 
 const analogiesData = [
     {
-        amountPerKwh: 0,
-        text: (value) => value,
-        asset: 'data.png'
+        text: (value) =>  {
+            const mbPerUnit = 20;
+            let number = roundToPrecision(value/mbPerUnit, 1);
+            return `${number} illustrated dictionaries`;
+        },
+        asset: 'analogy_dictionary.png'
     },
     {
-        amountPerKwh: 0,
-        text: (value) => value,
-        asset: 'data.png'
+        text: (value) =>  {
+            const mbPerUnit = 0.078;
+            let number = Math.floor(value / mbPerUnit);
+            return `${number} instagram pictures`;
+        },
+        asset: 'analogy_insta.png'
     },
     {
-        amountPerKwh: 0,
-        text: (value) => value,
-        asset: 'data.png'
+        text: (value) =>  {
+            const mbPerUnit = 2.4;
+            let number = Math.floor(value / mbPerUnit);
+            return `${number} minutes of musique streaming`;
+        },
+        asset: 'analogy_streaming.png'
     },
     {
-        amountPerKwh: 0,
-        text: (value) => value,
-        asset: 'data.png'
+        text: (value) =>  {
+            const mbPerUnit = 5376;
+            let number = roundToPrecision(value/mbPerUnit, 2);
+            return `${number} Netflix UHD episodes`;
+        },
+        asset: 'analogy_tvzomby.png'
     },
     {
-        amountPerKwh: 0,
-        text: (value) => value,
-        asset: 'data.png'
+        text: (value) =>  {
+            const mbPerUnit = 0.000011444;
+            let number = Math.floor(value / mbPerUnit);
+            if (number < 1000000) {
+                return `${number} pages of word file`;
+            }
+            return `${Math.ceil(number/1000000)} million pages of word file`;
+        },
+        asset: 'analogy_wordfile.png'
     },
     {
-        amountPerKwh: 0,
-        text: (value) => value,
-        asset: 'data.png'
+        text: (value) =>  {
+            const mbPerUnit = 8;
+            let number = Math.floor(value / mbPerUnit);
+            return `${number} old USB Stick (8 MB)`;
+        },
+        asset: 'analogy_usbdrive.png'
     }
 ]
 
@@ -120,7 +141,7 @@ export default {
             // analogies based on energy (switch energy MJ to kWh)
             amount = 0.278 * data.energy;
         }
-        return analogy.value.text(amount);
+        return analogy.value.text(amount / 1000000); // data in MB for analogies
     });
 
     return {asset, legend};
