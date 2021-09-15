dfc523a48d87b59909f7431f947f9630d7f2141a
Nicolas Chabloz <synn.void@gmail.com>
2021-09-15 14:09:41 +0200

    Heatmap: calculate the  max heat to  set color accordingly

--- webpack/src/composables/heatmapChart.js
+++ webpack/src/composables/heatmapChart.js
@@ -6,7 +6,6 @@ const colorCo2HeatLow = '#D1AD75';
 const colorCo2Heat = '#906C0D';
 const colorDataHeatLow = '#92A09F';
 const colorDataHeat = '#384E50';
-
 // const fontFamily = 'Roboto, Arial, sans-serif ';
 // const fontWeight = 900;
 
@@ -20,38 +19,52 @@ export default function (data, mode) {
   }
   categories.reverse();
 
-  const options =  {
-    chart: {
-      type: 'heatmap',
-      toolbar: {show: false},
-      redrawOnParentResize: false
-    },
-    dataLabels: { enabled: false },
-    legend: { show: false },
-    tooltip: {
-      enabled: true,
-      y: {
-        show: true,
-        formatter: val => mode == 'co2' ? formatCo2(val) : formatSize(val),
+  const options =  computed(() => {
+    // Get the highest value from the data to set it as the max heat
+    let max = 0;
+    for (const hour of  data.value) {
+      for (const value of hour) {
+        if (value>max) max = value;
       }
-    },
-    plotOptions: {
-      heatmap: {
-        colorScale: {
-          ranges: [
-            { from: 0, to: 0, color: colorEmptyHeat },
-            { from: 0.02, to: 0.025, color: mode == 'co2' ? colorCo2HeatLow : colorDataHeatLow},
-            { from: 0.025, to: 0.0305, color: mode == 'co2' ? colorCo2Heat : colorDataHeat},
-          ]
-        }
-      }
-    },
-    xaxis: {
-      type: 'category',
-      labels: { show: true },
-      categories
     }
-  };
+
+    return {
+      chart: {
+        type: 'heatmap',
+        toolbar: {show: true},
+        redrawOnParentResize: false
+      },
+      dataLabels: { enabled: false },
+      legend: { show: false },
+      tooltip: {
+        x: { show: true },
+        y: {
+          show: true,
+          formatter: val => mode == 'co2' ? formatCo2(val) : formatSize(val),
+        }
+      },
+      plotOptions: {
+        heatmap: {
+          shadeIntensity: 1,
+          radius: 0,
+          useFillColorAsStroke: true,
+          colorScale: {
+            ranges: [
+              { from: 0, to: 0, color: colorEmptyHeat },
+              { from: 0.001, to: max ? max/2 : 1, color: mode == 'co2' ? colorCo2HeatLow : colorDataHeatLow },
+              { from: max ? max/2 : 1, to: max ? max : 2, color: mode == 'co2' ? colorCo2Heat : colorDataHeat },
+              // { from: 0.001, to: max ? max : 2, color: mode == 'co2' ? colorCo2Heat : colorDataHeat},
+            ]
+          }
+        }
+      },
+      xaxis: {
+        type: 'category',
+        labels: { show: true },
+        categories
+      }
+    }
+  });
 
   const series = computed(() => Array.from({length: 24}, (v, i) => {
     let hStart = 23 - i;
--- webpack/src/composables/storage.js
+++ webpack/src/composables/storage.js
@@ -30,7 +30,6 @@ export const getCurWeek = async (mode = 'co2') => {
         const d = new Date(entry.index + ':00:00');
         const diffInTime = d.getTime() - date.getTime();
         const indDay = Math.ceil(diffInTime / 86400000) - 1 ;
-        console.log(indDay);
         byHours[entry.hour][indDay] = mode == 'co2' ? entry.co2 : entry.data;
     }
     return byHours;
